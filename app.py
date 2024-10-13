d775a5cfd1ed8db9d3e356d4d38578c0bc117d6d
mia-amanda <miakaze2710@gmail.com>
2024-10-13 14:38:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Oracle Cloud! Running on Compute Instance~'
+    return 'Hello, Oracle Cloud! Running on Compute Instance!!!!!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
